b0d817737949b75e869df8ead6feff1b4f34feb6
ShahinAlomWD <81849648+ShahinAlomWD@users.noreply.github.com>
2021-04-03 13:22:23 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.autoTyping', {
+    strings: [
+        'Professional Web designer',
+        'and developer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    smartBackspace: true,
+    loop: true,
+    backDelay: 1500,
+    cursorChar: '|', // na dilewo colbe
+  });
